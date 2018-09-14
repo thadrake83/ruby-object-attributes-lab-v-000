ff79092e8d14049f84f2798df5187587d438d813
Drake Rentrop <drakerentrop@gmail.com>
2018-09-14 19:16:01 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Person
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+ 
+  def breed 
+    @breed 
+  end
+  
+   def job=(persons_job)
+    @job = persons_job
+  end
+ 
+  def job
+    @job
+  end
+  
+end
